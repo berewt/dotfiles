19ff0d1df1a87d284a00dfb5418411c18c5f7121
Nicolas Biri <nicolas@biri.name>
2025-12-22 13:17:41 +0100

    Add idris support and nix formatter

--- .config/nvim/lua/plugins/unison.lua
+++ .config/nvim/lua/plugins/unison.lua
@@ -4,7 +4,6 @@ return {
     branch = "trunk",
     config = function(plugin)
       vim.opt.rtp:append(plugin.dir .. "/editor-support/vim")
-      require("lazy.core.loader").packadd(plugin.dir .. "/editor-support/vim")
     end,
     init = function(plugin)
       require("lazy.core.loader").ftdetect(plugin.dir .. "/editor-support/vim")
